dd5fd70d1e7623136bbfddd0faf7090a6b74ec9e
Ekaterina Volynshchikova <loresina86@gmail.com>
2023-06-10 16:44:50 +0700

    fix cli

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const gameLevel = (theQuestion, createQuestion, getRightAnswer) => {
   // greeting
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 
   // question
   console.log(theQuestion);
