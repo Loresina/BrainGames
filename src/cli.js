8060340d9a94e080305c096045187c416a3b67a2
Ekaterina Volynshchikova <loresina86@gmail.com>
2023-06-26 15:39:16 +0700

    fix brain-games

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const questions = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 };
 
 export default questions;
